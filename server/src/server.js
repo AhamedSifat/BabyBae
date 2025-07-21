8cb1ed8ff3eb0ec67e5f999ade5e923db1617078
sifat1234 <ahahmedsifat111@gmail.com>
2025-07-21 17:14:13 +0600

    add MongoDB connection using Mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
