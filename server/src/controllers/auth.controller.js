26eff9791ea9609373fe5a711ddc2afd1ffaad75
sifat1234 <ahahmedsifat111@gmail.com>
2025-07-21 17:56:33 +0600

    enable login functionality with credential validation

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -34,3 +34,23 @@ export const registerUser = asyncHandler(async (req, res) => {
     throw new Error('Invalid user data');
   }
 });
+
+export const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+      role: user.role,
+      addresses: user.addresses || [],
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
