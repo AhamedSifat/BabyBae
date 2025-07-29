a4ee48c5bc9866c5923be367ea3c2b07681c0dab
sifat1234 <ahahmedsifat111@gmail.com>
2025-07-29 15:55:39 +0600

    implement protected layout with Sidebar, Header, and route outlet

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,5 +1,30 @@
+import { Navigate, Outlet } from 'react-router';
+import Header from './components/common/Header';
+import Sidebar from './components/dashboard/Sidebar';
+import { cn } from './lib/utils';
+
 const App = () => {
-  return <div>App</div>;
+  const isAuthenticated = true;
+
+  if (!isAuthenticated) {
+    return <Navigate to='/login' />;
+  }
+  return (
+    <div className='h-screen flex bg-background'>
+      <Sidebar />
+      <div
+        className={cn(
+          'flex-1 flex flex-col max-w-[--breakpoint-2xl] hoverEffect ml-64'
+        )}
+      >
+        <Header />
+
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -3,8 +3,20 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { createBrowserRouter, RouterProvider } from 'react-router';
+import Login from './pages/Login.tsx';
+import Register from './pages/Register.tsx';
 
 const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <Login />,
+  },
+
+  {
+    path: '/register',
+    element: <Register />,
+  },
+
   {
     path: '/',
     element: <App />,
