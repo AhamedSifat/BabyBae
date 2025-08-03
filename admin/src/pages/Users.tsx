08ea296a830ae6ed9b962e8abbc05ccb48fd04f3
sifat1234 <ahahmedsifat111@gmail.com>
2025-08-03 15:51:38 +0600

    fetch users

--- admin/src/pages/Users.tsx
+++ admin/src/pages/Users.tsx
@@ -1,7 +1,27 @@
-import { useState } from 'react';
+import { useAxiosPrivate } from '@/hooks/use-axios-private';
+import { useEffect, useState } from 'react';
 
 const Users = () => {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const axiosPrivate = useAxiosPrivate();
+
+  const fetchUsers = async () => {
+    setLoading(true);
+    try {
+      const response = await axiosPrivate.get('/users');
+      console.log('response', response);
+      setUsers(response.data);
+    } catch (error) {
+      console.log('Failed to load users', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   return <div>Users</div>;
 };
