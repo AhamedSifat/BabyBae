87e6e495a1e51f069294e554784925ca04759508
sifat1234 <ahahmedsifat111@gmail.com>
2025-08-08 21:30:56 +0600

    add deleteUser controller to remove user by ID with error handling

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -31,3 +31,13 @@ export const createUser = asyncHandler(async (req, res) => {
     res.status(500).json({ message: 'User creation failed' });
   }
 });
+export const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findByIdAndDelete(req.params.id);
+
+  if (!user) {
+    res.status(404);
+    throw new Error('User not found');
+  }
+
+  res.status(200).json({ message: 'User removed successfully' });
+});
--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import { protect, admin } from '../middleware/auth.middleware.js';
-import { createUser, getUsers } from '../controllers/user.controller.js';
+import {
+  createUser,
+  getUsers,
+  deleteUser,
+} from '../controllers/user.controller.js';
 
 const userRouter = express.Router();
 
 userRouter.get('/', protect, admin, getUsers);
 userRouter.post('/', protect, admin, createUser);
+userRouter.delete('/:id', protect, admin, deleteUser);
 export default userRouter;
